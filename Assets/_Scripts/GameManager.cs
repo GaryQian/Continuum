7c583aae928436a95d19406cd2d56a611f51b96c
hallagon <anthonysunghoonkim@gmail.com>
2018-02-14 01:14:22 -0500

    [anthony]incorporated gary's GameManager. pushing to anthony branch first

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,35 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Singleton. Access with GameManager.Instance
+/// Handles overall game flow such as starting game, and delegating to local managers.
+/// </summary>
 public class GameManager : MonoBehaviour {
 
-    public static GameManager Instance;
-    //public static int saveVersion;
-    //public SettingsData settings;
-//	public GameObject FPSController;
+	public static GameManager Instance; ///Singleton Instance
+
+	public static int saveVersion;
+	public SettingsData settings;
+
 	public GameObject player;
 	public Transform playerLocation;
-	public float px, py, pz;
 
-    private void Awake() {
-        if (Instance != null) {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
+	/// <summary>
+	/// EARLY setup that is done before the start function.
+	/// </summary>
+	private void Awake() {
+		if (Instance != null) {
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;
 		playerLocation = player.transform;
-    }
-
-    // Use this for initialization
-    void Start () {
-        //saveVersion = SaveManager.Instance.GetVersion();
-        //SaveManager.Instance.SaveVersion();
-
-        //settings = SaveManager.Instance.LoadSettings();
-        
 	}
-	
-	// Update is called once per frame
+
+	/// <summary>
+	/// Load everything that needs to be done at game load.
+	/// </summary>
+	void Start () {
+		saveVersion = SaveManager.Instance.GetVersion();
+		SaveManager.Instance.SaveVersion();
+
+		settings = SaveManager.Instance.LoadSettings();
+
+	}
+
+	/// <summary>
+	/// Global update. Run on every frame. Try to put things in more local update loops, but some things like global counters can go here.
+	/// </summary>
 	void Update () {
 		playerLocation = player.transform;
 	}
