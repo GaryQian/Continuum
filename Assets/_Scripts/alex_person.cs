a31410be14e79f6480148735d4e1d80da204eecc
Alex Ahn <alexahn917@gmail.com>
2018-02-14 00:09:34 -0500

    [alex] sandbox scene implemented

---
+++ Assets/_Scripts/alex_person.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alex_person : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        Debug.Log("alex_person script");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag.Equals("Cylinder")) {
+            Destroy(other.gameObject);
+        }
+    }
+}
