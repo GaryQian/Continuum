64a557bbfd76969d8423ed8e8e9b15f88be72715
hallagon <anthonysunghoonkim@gmail.com>
2018-02-14 01:25:41 -0500

    rebasing2

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -8,42 +8,37 @@ using UnityEngine;
 /// Handles overall game flow such as starting game, and delegating to local managers.
 /// </summary>
 public class GameManager : MonoBehaviour {
+	public static GameManager Instance; ///Singleton Instance
 
-    public static GameManager Instance; ///Singleton Instance
+	public static int saveVersion;
+	public SettingsData settings;
 
-    public static int saveVersion;
-    public SettingsData settings;
-    public static GameManager Instance;
 	public GameObject player;
 	public Transform playerLocation;
 	public float px, py, pz;
 
-    /// <summary>
-    /// EARLY setup that is done before the start function.
-    /// </summary>
-    private void Awake() {
-        if (Instance != null) {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-		playerLocation = player.transform;
-    }
-
-    /// <summary>
-    /// Load everything that needs to be done at game load.
-    /// </summary>
-    void Start () {
-        //saveVersion = SaveManager.Instance.GetVersion();
-        //SaveManager.Instance.SaveVersion();
-
-        //settings = SaveManager.Instance.LoadSettings();
-        
-	}
-	
 	/// <summary>
-    /// Global update. Run on every frame. Try to put things in more local update loops, but some things like global counters can go here.
-    /// </summary>
+	/// EARLY setup that is done before the start function.
+	/// </summary>
+	private void Awake() {
+		if (Instance != null) {
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;
+		playerLocation = player.transform;
+	}
+
+	/// <summary>
+	/// Load everything that needs to be done at game load.
+	/// </summary>
+	void Start () {
+		saveVersion = SaveManager.Instance.GetVersion ();
+		SaveManager.Instance.SaveVersion ();
+	}
+	/// <summary>
+	/// Global update. Run on every frame. Try to put things in more local update loops, but some things like global counters can go here.
+	/// </summary>
 	void Update () {
 		playerLocation = player.transform;
 	}
