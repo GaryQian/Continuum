6d7f0cf401c70fb143d3730cffec1472389f436e
Julian Orillac <julianorillac@Julians-MacBook-Pro.local>
2018-02-21 15:29:19 -0500

    made doors

---
+++ Assets/_Scripts/Doors.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+
+	Animator animator;
+	bool doorOpen;
+
+	void Start() {
+		doorOpen = false;
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			doorOpen = true;
+			ControlDoors("Open");
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (doorOpen) {
+			doorOpen = false;
+			ControlDoors("Close");
+		}
+	}
+
+	void ControlDoors(string direction) {
+		animator.SetTrigger(direction);
+	}
+}
---
+++ Assets/_Scripts/PressureDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureDoor : MonoBehaviour {
+
+	Animator animator;
+	bool doorOpen;
+	double timeLeft = 0;
+
+	void Start() {
+		doorOpen = false;
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			doorOpen = true;
+			ControlDoors ("Open");
+			timeLeft = 5.0;
+		}
+	}
+
+	void Update() {
+		timeLeft -= Time.deltaTime;
+		if (timeLeft < 0 && doorOpen) {
+			doorOpen = false;
+			ControlDoors("Close");
+		}
+	}
+
+	void ControlDoors(string direction) {
+		animator.SetTrigger(direction);
+	}
+}
