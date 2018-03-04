f172bf86e7e28c09b79e89a3193f154c4aa74596
Matt <msaltzm5@jhu.edu>
2018-03-04 16:48:04 -0500

    turrets v2 and v3

---
+++ Assets/_Scripts/EmptyClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretRotationModule : MonoBehaviour {
+
+	public GameManager gm;
+
+	void Awake () {
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gm.player)
+		{
+			transform.rotation = Quaternion.LookRotation(gm.player.transform.position - transform.position);
+		}
+	}
+
+	void FixedUpdate(){
+
+	}
+
+
+}
---
+++ Assets/_Scripts/TurretCapsuleModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretCapsuleModule : MonoBehaviour {
+
+	public GameManager gm;
+	public Quaternion rotationQuat;
+
+	void Awake () {
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gm.player)
+		{
+			transform.rotation = Quaternion.LookRotation(gm.player.transform.position - transform.position);
+		}
+	}
+
+	void FixedUpdate(){
+
+	}
+
+
+}
