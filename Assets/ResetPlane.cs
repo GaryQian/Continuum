79cb7f3b7b84e5f9c3f5076d76a9a24616a05ee0
jorilla1 <jorilla1@jhu.edu>
2018-03-06 17:25:05 -0500

    added reset plane in title

---
+++ Assets/ResetPlane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlane : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //kills object through health so anything that would happen on die would happen
+        if (other.tag == "Player")
+        {
+            Transform transform = other.GetComponent<Transform>();
+            Vector3 vec = new Vector3(0, 0, 0);
+            transform.SetPositionAndRotation(vec, Quaternion.identity);
+        }
+    }
+}
