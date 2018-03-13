58e6f86f0e7b509ea7a6842b3d6100f962a15c92
Gary Qian <gqian1@jhu.edu>
2018-03-13 10:03:20 -0400

    Postprocessing

---
+++ Assets/_Materials/Neon/InsideRender.shader
@@ -0,0 +1,31 @@
+﻿Shader "Custom/InsideRender" {
+     SubShader {
+       
+       Tags { "RenderType" = "Transparent" }
+       
+       Cull Front
+       
+       CGPROGRAM
+       
+       #pragma surface surf Lambert vertex:vert
+       
+       void vert(inout appdata_full v)
+       {
+           v.normal.xyz = v.normal * -1;
+       }
+       
+       struct Input {
+           float4 color : COLOR;
+       };
+       
+       void surf (Input IN, inout SurfaceOutput o) {
+           o.Albedo = 1;
+       }
+       
+       ENDCG
+       
+     }
+     
+     Fallback "Diffuse"
+     
+   }
---
+++ Assets/_Scripts/Graphics/BloomOnSwictch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BloomOnSwictch : MonoBehaviour {
+    UnityEngine.Rendering.PostProcessing.PostProcessVolume vol;
+
+    float curr;
+    public float duration;
+    float startTime;
+    // Use this for initialization
+    void Start () {
+        vol = GetComponent<UnityEngine.Rendering.PostProcessing.PostProcessVolume>();
+        StartCoroutine("Routine");
+	}
+	
+	IEnumerator Routine() {
+        startTime = Time.time;
+        while (Time.time - startTime < duration) {
+            vol.weight = Mathf.Lerp(1.0f, 0, (Time.time - startTime) / duration);
+            yield return null;
+        }
+        vol.weight = 0;
+    }
+}
