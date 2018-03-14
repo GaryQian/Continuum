c5717cd9957e2607583635c93b85802c653a5307
jorilla1 <jorilla1@jhu.edu>
2018-03-14 16:56:04 -0400

    added suicide

---
+++ Assets/_Scripts/Suicider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suicider : MonoBehaviour {
+
+    public float time;
+	// Use this for initialization
+	void Start () {
+        Invoke("Die", time);
+	}
+	
+	// Update is called once per frame
+    void Die () {
+        Destroy(gameObject);
+    }
+}
