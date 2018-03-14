7cbaaecba631c979c83cc94e2f4411a0ebbb299c
Alex Ahn <alexahn917@gmail.com>
2018-03-14 16:11:51 -0400

    [laser] added laser component!

---
+++ Assets/_Scripts/Laser/LaserScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour
+{
+    public GameManager gm;
+    public LineRenderer laserLineRenderer;
+    public float laserMaxLength = 10f;
+    public float laserDPS = 100.0f;
+    public GameObject laser;
+    public Transform target;
+    public Vector3 dist;
+
+    void Start() {
+        laserLineRenderer = this.gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        castLaser();
+    }
+
+    void castLaser() {
+        dist = target.position - laser.transform.position;
+        laserLineRenderer.SetPosition(0, laser.transform.position);
+        laserLineRenderer.SetPosition(1, DetectHit(laser.transform.position, laserMaxLength, dist));
+    }
+
+    Vector3 DetectHit(Vector3 startPos, float distance, Vector3 direction)
+    {
+        Ray ray = new Ray(startPos, direction);
+        RaycastHit hit;
+        Vector3 endPos = startPos + (distance * direction);
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            endPos = hit.point;
+            if (hit.transform.gameObject.CompareTag("Player"))
+            {
+                damagePlayer();
+            }
+            return endPos;
+        }
+
+        return endPos;
+    }
+
+    void damagePlayer() {
+        // GameManager.Instance.player.damage(laserDamageDPS);
+    }
+}
---
+++ Assets/_Scripts/Laser/MoveCircular.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCircular : MonoBehaviour {
+
+    public Transform laser;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = new Vector3(0, 0, 0);
+        Vector3 relativePos = (laser.position + offset) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        Quaternion current = transform.localRotation;
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime);
+	}
+}
