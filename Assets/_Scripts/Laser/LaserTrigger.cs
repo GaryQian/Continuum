3dc8a4e250e3d59c1d7b0a52d379cc2472dd6b79
Alex Ahn <alexahn917@gmail.com>
2018-03-25 12:46:50 +0900

    laser set with pressure plate implemented

---
+++ Assets/_Scripts/Laser/LaserTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserTrigger : MonoBehaviour {
+
+    public GameObject[] laserBeams;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            foreach(GameObject laserBeam in laserBeams) {
+                laserBeam.SetActive(false);
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player")
+        {
+            foreach (GameObject laserBeam in laserBeams)
+            {
+                laserBeam.SetActive(true);
+            }
+
+        }
+    }
+}
