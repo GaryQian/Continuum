2bde90e9a87f79e0cad01fcd8f12bf0b1a4734da
hallagon <anthonysunghoonkim@gmail.com>
2018-04-08 18:41:08 -0400

    tutorial level
    Former-commit-id: 1fabe72d528adc7c477cdcca64da7fa3e623a131

---
+++ Assets/_Scripts/AIMovement/DestinationChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestinationChanger : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public List<GameObject> DestinationList;
+	private int count, index;
+	// Use this for initialization
+	void Start () {
+		index = 0;
+		agent.SetDestination(DestinationList [index].transform.position);
+		count = DestinationList.Count;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((gameObject.transform.position - DestinationList [index].transform.position).magnitude < 1f) {			
+			agent.SetDestination (DestinationList [calculateIndex()].transform.position);
+		}
+
+	}
+
+	int calculateIndex(){
+		if (index++ >= DestinationList.Count - 1)
+			index = 0;
+		return index;
+	}
+
+
+}
---
+++ Assets/_Scripts/DashableWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DashableWall : MonoBehaviour {
+
+	public GameObject player;
+	private BoxCollider wallCollider;
+	private FirstPersonController fpsScript;
+
+	// Use this for initialization
+	void Start () {
+		wallCollider = gameObject.GetComponent<BoxCollider> ();
+		fpsScript = player.GetComponent<FirstPersonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fpsScript.isDashing_delayed) {
+			wallCollider.enabled = false;;
+		} else {
+			wallCollider.enabled = true;
+		}
+	}
+}
