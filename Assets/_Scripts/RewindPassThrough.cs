1ea3ab484795ede2496bf047b319fe78048838c4
hallagon <anthonysunghoonkim@gmail.com>
2018-04-08 20:15:40 -0400

    Tutorial level - glass barricade for rewind
    Former-commit-id: abe68b6ee526e9a0994f23304a97461085610708

---
+++ Assets/_Scripts/RewindPassThrough.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class RewindPassThrough : MonoBehaviour {
+
+	private BoxCollider boxCollider;
+	// Use this for initialization
+	void Start () {
+		boxCollider = gameObject.GetComponent<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PuppetRegister.inRewind) {
+			boxCollider.enabled = false;
+		} else {
+			boxCollider.enabled = true;
+		}
+	}
+}
