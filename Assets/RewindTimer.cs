5df976cd90ae86ff31d9d9eb06f87b98cf9079f6
jorilla1 <jorilla1@jhu.edu>
2018-04-08 20:18:49 -0400

    adding radial timer
    Former-commit-id: baf368eae52a471a960ea5b2a31eafda71d444ad

---
+++ Assets/RewindTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewindTimer : MonoBehaviour {
+
+
+    public Image LoadingBar;
+    public Text Textedit;
+
+    public float MaxSound = 10f;
+    private float curSound = 0f;
+
+	// Update is called once per frame
+	void Update () {
+        curSound = PuppetRegister.timer;
+        if (curSound <= MaxSound) {
+            Textedit.text = curSound.ToString() + "%";
+        } else {
+            Textedit.text = "Ready!";
+        }
+
+        LoadingBar.fillAmount = curSound / MaxSound;
+	}
+}
