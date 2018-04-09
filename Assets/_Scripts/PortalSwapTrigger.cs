a100c8edaad54f09473c881f5921ec1b37baa26f
hallagon <anthonysunghoonkim@gmail.com>
2018-04-08 21:38:31 -0400

    portals work
    Former-commit-id: e7d75e40f5b09609f2ab531c7d4c86f95cb772fd

---
+++ Assets/_Scripts/PortalSwapTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSwapTrigger : MonoBehaviour {
+
+	public GameObject originalModel, portalModel;
+	private bool destroyComplete = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!destroyComplete && PuppetRegister.inRewind && other.gameObject.tag.Equals ("Player") && other.gameObject.GetComponent<Puppet>() == null) {
+			Destroy (originalModel);
+			portalModel.SetActive (true);
+		}
+	}
+}
