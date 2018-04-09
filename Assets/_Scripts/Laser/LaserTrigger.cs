ce1fd5b8d9108a8dd2f84c024148ba79bac22020
Gary Qian <gqian1@jhu.edu>
2018-04-09 00:37:47 -0400

    Blue set play working :)
    Former-commit-id: 6092613a4381508a091186ea7b9860e81e0547ad

--- Assets/_Scripts/Laser/LaserTrigger.cs
+++ Assets/_Scripts/Laser/LaserTrigger.cs
@@ -6,33 +6,49 @@ public class LaserTrigger : MonoBehaviour {
 
     public GameObject[] laserBeams;
 
+
+    static List<GameObject> players;
+
 	// Use this for initialization
 	void Start () {
-		
+        players = new List<GameObject>();
+        InvokeRepeating("Check", 1f, 0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    void Check() {
+        int count = 0;
+        foreach (GameObject obj in players) {
+            if (obj == null) count++;
+        }
+        Debug.Log("" + players.Count + " " + count);
+        if (players.Count - count <= 0) {
+            Debug.Log("Reactivating Lasers");
+            foreach (GameObject laserBeam in laserBeams) {
+                laserBeam.SetActive(true);
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player")
         {
+            Debug.Log("Triggered Laser!");
+            players.Add(other.gameObject);
             foreach(GameObject laserBeam in laserBeams) {
                 laserBeam.SetActive(false);
             }
-
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.tag == "Player")
         {
-            foreach (GameObject laserBeam in laserBeams)
-            {
-                laserBeam.SetActive(true);
-            }
+            players.Remove(other.gameObject);
+            Check();
 
         }
     }
---
+++ Assets/_Scripts/Radio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour {
+
+    public AudioSource source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy() {
+        source.Stop();
+    }
+}
