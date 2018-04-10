80eb8b3ab75dace1eae89d9c86a0f6d8988e9d3e
Gary Qian <gqian1@jhu.edu>
2018-04-10 17:50:36 -0400

    Respwan screen
    Former-commit-id: d7311807923149af8ac153c53868b88ab24e86a9

--- Assets/_Scripts/KillScreen.cs
+++ Assets/_Scripts/KillScreen.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KillScreen : MonoBehaviour {
 
+    public string levelName;
     Health health;
 
     private void OnTriggerEnter(Collider other)
     {
         //kills object through health so anything that would happen on die would happen
-        if ((health = other.GetComponent<Health>()) != null) {
-            health.Damage(100000);
+        if ((health = other.GetComponent<Health>()) != null && other.gameObject.tag == "Player") {
+            SceneManager.LoadScene(levelName);
+            //health.Damage(100000);
         } else {
             Destroy(other);
         }
