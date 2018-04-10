f80abaf12d9bcb34cc4af2e9e749bfeafa5ebd06
jorilla1 <jorilla1@jhu.edu>
2018-04-10 10:27:23 -0400

    made gauge again
    Former-commit-id: 1246169b1fd888d90cd1ab9c60f48a4358d1d4b6

---
+++ Assets/TimerGauge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGauge : MonoBehaviour {
+
+    private float curTimer;
+    public float maxTimer = 10f;
+
+    public Text text;
+    public Image fill;
+
+	// Update is called once per frame
+	void Update () {
+        curTimer = PuppetRegister.timer;
+
+        if (curTimer <= maxTimer) {
+            text.text = curTimer + "%";
+            fill.fillAmount = curTimer / maxTimer;
+        } else {
+            text.text = "Ready!";
+            fill.fillAmount = 1;
+        }
+	}
+}
