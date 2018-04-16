4ff25005b8e4920111230fd76875c715d43854e0
Gary Qian <gqian1@jhu.edu>
2018-04-16 02:30:33 -0400

    Story radio
    Former-commit-id: 44687f94f496ad19b16a1c0ad2dc5206a45a43e4

--- Assets/_Scripts/Radio.cs
+++ Assets/_Scripts/Radio.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Radio : MonoBehaviour {
 
-    public AudioSource source;
+    AudioSource source;
+    public AudioClip clip;
+    public bool loop;
+    public float delay = 0;
 	// Use this for initialization
 	void Start () {
-		
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = loop;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player" && !source.isPlaying) {
+            Invoke("StartSound", delay);
+        }
+    }
+
+    void StartSound() {
+        source.Play();
+        CancelInvoke("StartSound");
+    }
 
     private void OnDestroy() {
         source.Stop();
