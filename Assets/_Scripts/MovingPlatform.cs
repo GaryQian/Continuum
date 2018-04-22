54637f3667355e8241e9097582e340ba965ca6cc
Gary Qian <gqian1@jhu.edu>
2018-04-22 19:42:54 -0400

    Editor buttons to help set stuff
    Former-commit-id: 8e950d099096596e8b668cf4474140e8fa79e4bc
    Former-commit-id: eb04d301267c1d6bec801308f0b6854d76f72a42

--- Assets/_Scripts/MovingPlatform.cs
+++ Assets/_Scripts/MovingPlatform.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MovingPlatform : MonoBehaviour {
-
-
     public Vector3 endPos;
-    Vector3 startPos;
+    public Vector3 startPos;
+    public bool autoSetStart = false;
     public float duration = 5f;
     float t = 0;
     bool dir = true;
@@ -14,7 +14,7 @@ public class MovingPlatform : MonoBehaviour {
     public Rigidbody body;
 	// Use this for initialization
 	void Start () {
-        startPos = transform.position;
+        if (autoSetStart) startPos = transform.position;
         t = (initOffset % duration) / duration;
         body = GetComponent<Rigidbody>();
 	}
@@ -33,5 +33,44 @@ public class MovingPlatform : MonoBehaviour {
         else body.MovePosition(new Vector3(Mathf.SmoothStep(endPos.x, startPos.x, offset),
                                                Mathf.SmoothStep(endPos.y, startPos.y, offset),
                                                Mathf.SmoothStep(endPos.z, startPos.z, offset)));
+    }
+
+    public void SetStart() {
+        startPos = transform.position;
+    }
+
+    public void SetEnd() {
+        endPos = transform.position;
+    }
+
+    public void GoToStart() {
+        transform.position = startPos;
+    }
+
+    public void GoToEnd() {
+        transform.position = endPos;
+    }
+}
+
+[CustomEditor(typeof(MovingPlatform))]
+public class MovingPlatformEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        MovingPlatform myScript = (MovingPlatform)target;
+        if (GUILayout.Button("Set Start")) {
+            myScript.SetStart();
+        }
+
+        if (GUILayout.Button("Set End")) {
+            myScript.SetEnd();
+        }
+
+        if (GUILayout.Button("Go To Start")) {
+            myScript.GoToStart();
+        }
+        if (GUILayout.Button("Go To End")) {
+            myScript.GoToEnd();
+        }
     }
 }
