858d7281d00cee369facd4a6e43a0596b3602f50
Gary Qian <gqian1@jhu.edu>
2018-05-08 13:04:05 -0400

    Quick new UI

--- Assets/_Scripts/UIBars.cs
+++ Assets/_Scripts/UIBars.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class UIBars : MonoBehaviour {
 
-    private Slider healthBar;
-    private Slider staminaBar;
+    private Image healthBar;
+    private Image staminaBar;
 	// Use this for initialization
 	void Start () {
-        healthBar = (Slider)GameObject.Find("HealthBar").GetComponent(typeof(Slider));
-        staminaBar = (Slider)GameObject.Find("StaminaBar").GetComponent(typeof(Slider));
+        healthBar = (Image)GameObject.Find("HealthBar2").GetComponent(typeof(Image));
+        staminaBar = (Image)GameObject.Find("StaminaBar2").GetComponent(typeof(Image));
 	}
 	
     //sets the bars on the HUD to the desired value
     public void SetHealthBar(float cur, float max) {
-        if (healthBar != null) healthBar.value = cur / max;
+        if (healthBar != null) healthBar.fillAmount = cur / max;
     }
 
     public void SetStaminaBar(float cur, float max) {
-        if (staminaBar != null) staminaBar.value = cur / max;
+        if (staminaBar != null) staminaBar.fillAmount = cur / max;
     }
 }
