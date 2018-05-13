29bc6a6d073fd74feaf72a1bb80fd99a716dcf81
hallagon <anthonysunghoonkim@gmail.com>
2018-05-13 15:44:27 -0400

    Tutorial level portal wall redone

--- Assets/_Scripts/RewindPassThrough.cs
+++ Assets/_Scripts/RewindPassThrough.cs
@@ -7,17 +7,20 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class RewindPassThrough : MonoBehaviour {
 
 	private BoxCollider boxCollider;
+    private Renderer glassRenderer;
 	// Use this for initialization
 	void Start () {
 		boxCollider = gameObject.GetComponent<BoxCollider> ();
+        glassRenderer = gameObject.GetComponent<MeshRenderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (PuppetRegister.inRewind) {
 			boxCollider.enabled = false;
+            glassRenderer.enabled = false;
 		} else {
-			boxCollider.enabled = true;
+			//boxCollider.enabled = true;
 		}
 	}
 }
