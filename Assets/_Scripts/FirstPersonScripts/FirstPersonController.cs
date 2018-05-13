c10cc1627edac11eddf4cd0bdcc68fe48ffcf915
jorilla1 <jorilla1@jhu.edu>
2018-05-12 21:26:50 -0400

    fixed double jump issue

--- Assets/_Scripts/FirstPersonScripts/FirstPersonController.cs
+++ Assets/_Scripts/FirstPersonScripts/FirstPersonController.cs
@@ -118,7 +118,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
 			RotateView();
 			// the jump state needs to read here to make sure it is not missed
-			if (!m_Jump)
+			if (!m_Jump && !m_Jumping)
 			{
 				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
 			}
